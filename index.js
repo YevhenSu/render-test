ce6997ed17425e895c3132371c798e1b16680a78
YevhenSu <susidka.yevhen@gmail.com>
2023-10-04 13:34:10 +0300

    modified:   .gitignore 	modified:   index.js 	modified:   package-lock.json 	modified:   package.json

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require( "express" )
 const mongoose = require( "mongoose" )
+const Note = require( "./models/note" ) 
 const cors = require( "cors" )
 const password = process.argv[2]
 
