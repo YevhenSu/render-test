827a44d5dfea65f772134de39edc9fee5d114382
YevhenSu <susidka.yevhen@gmail.com>
2023-10-04 12:39:12 +0300

    modified:   .gitignore 	modified:   index.js mongoose db code was added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require( "express" )
+const mongoose = require( "mongoose" )
 const cors = require( "cors" )
+const password = process.argv[2]
 
 const app = express()
 
@@ -21,23 +23,26 @@ app.use( express.json() )
 app.use( requestLogger )
 //app.use( unknownEndpoint )
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
+const url =
+  `mongodb+srv://susidkayevhen:${password}@cluster0.ddmeb4r.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+noteSchema.set( "toJSON", {
+	transform: ( document, returnedObject ) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	}
+} )
+
+const Note = mongoose.model('Note', noteSchema)
 
 app.get( "/", ( request, response ) => {
 	response.send( "<h1>Hello World!</h1>" )
@@ -95,7 +100,9 @@ app.post(
 )
 
 app.get( "/api/notes", ( request, response ) => {
-	response.json( notes )
+	Note.find({}).then( notes => {
+		response.json( notes )
+	} )
 } )
 
 const PORT = process.env.PORT || 3001
