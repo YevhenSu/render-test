31d139bcb46504f8323cafef9ce6e1a137dfdff9
YevhenSu <susidka.yevhen@gmail.com>
2023-10-06 12:51:45 +0300

    modified:   index.js app.put() route was added

--- index.js
+++ index.js
@@ -46,6 +46,25 @@ app.get(
 	}
 )
 
+app.put(
+	"/api/notes/:id", 
+	( request, response, next ) => {
+		const body = request.body
+
+		const note = {
+			content: body.content,
+			important: body.important,
+		}
+	
+		Note.findByIdAndUpdate( request.params.id, note, { new: true } )
+			.then( updatedNote => {
+				response.json( updatedNote )
+			} )
+			.catch( error => next( error ) )
+
+	}
+)
+
 app.delete(
 	"/api/notes/:id",
 	( request, response, next ) => {
