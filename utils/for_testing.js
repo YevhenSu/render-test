7dc4a835320d110490bdf21ed8b891c5fb0bd683
YevhenSu <susidka.yevhen@gmail.com>
2023-10-12 12:43:58 +0300

    -  return array.reduce( reducer, 0 ) / array.length +  return array.length === 0 ? 0 : array.reduce( reducer, 0 ) / array.length
    	modified:   utils/for_testing.js

--- utils/for_testing.js
+++ utils/for_testing.js
@@ -7,7 +7,7 @@ const reverse = string => {
 
 const average = array => {
   const reducer = ( acc, val ) => acc + val
-  return array.reduce( reducer, 0 ) / array.length
+  return array.length === 0 ? 0 : array.reduce( reducer, 0 ) / array.length
 }
 
 module.exports = {
