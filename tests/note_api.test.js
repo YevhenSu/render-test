8cc83ab5a411b49d427334add9117b93072718b1
YevhenSu <susidka.yevhen@gmail.com>
2023-10-14 15:21:37 +0300

    code was refactored inside:
    	modified:   tests/note_api.test.js

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,25 +1,17 @@
-const mongoose = require( 'mongoose' )
 const supertest = require( 'supertest' )
+const mongoose = require( 'mongoose' )
+const helper = require( './test_helper' )
 const app = require( '../app' )
 const api = supertest( app )
 const Note = require( '../models/note' )
 
-const initialNotes = [
-  {
-    content: 'HTML is Easy',
-    important: false,
-  },
-  {
-    content: 'Browser can execute only JavaScript',
-    important: true,
-  },
-]
-
 beforeEach( async () => {
   await Note.deleteMany( {} )
-  let noteObject = new Note( initialNotes[ 0 ] )
+
+  let noteObject = new Note( helper.initialNotes[ 0 ] )
   await noteObject.save()
-  noteObject = new Note( initialNotes[ 1 ] )
+
+  noteObject = new Note( helper.initialNotes[ 1 ] )
   await noteObject.save()
 } )
 
@@ -34,7 +26,7 @@ test('all notes are returned', async () => {
   const response = await api.get('/api/notes')
 
 
-  expect(response.body).toHaveLength(initialNotes.length)
+  expect(response.body).toHaveLength(helper.initialNotes.length)
 })
 
 
@@ -43,6 +35,7 @@ test('a specific note is within the returned notes', async () => {
 
 
   const contents = response.body.map(r => r.content)
+
   expect(contents).toContain(
     'Browser can execute only JavaScript'
   )
@@ -62,12 +55,12 @@ test(
       .expect( 201 )
       .expect( 'Content-Type', /application\/json/ )
 
-    const response = await api.get( '/api/notes' )
+    const notesAtEnd = await helper.notesInDb()
+    expect( notesAtEnd ).toHaveLength( helper.initialNotes.length + 1 )
 
-    const contents = response.body.map( r => r.content )
+    const contents = notesAtEnd.map( n => n.content )
 
-    expect( response.body ).toHaveLength( initialNotes.length + 1 )
-    expect( contents ).toContain(
+    expect(contents).toContain(
       'async/await simplifies making async calls'
     )
   }
@@ -85,9 +78,9 @@ test(
       .send( newNote )
       .expect( 400 )
 
-    const response = await api.get( '/api/notes' )
+    const notesAtEnd = await helper.notesInDb()
 
-    expect( response.body ).toHaveLength( initialNotes.length )
+    expect(notesAtEnd).toHaveLength( helper.initialNotes.length )
   }
 )
 
