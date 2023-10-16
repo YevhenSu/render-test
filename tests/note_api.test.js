83d238079e3211ffe442c64970104e500ef3f3b8
YevhenSu <susidka.yevhen@gmail.com>
2023-10-16 16:18:33 +0300

    beforeEach() was rewritten with insertMany() method
    	modified:   tests/note_api.test.js

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -5,15 +5,10 @@ const app = require( '../app' )
 const api = supertest( app )
 const Note = require( '../models/note' )
 
-beforeEach( async () => {
-  await Note.deleteMany( {} )
-
-  const noteObjects = helper.initialNotes.map(
-    note => new Note( note )
-  )
-  const promiseArray = noteObjects.map( note => note.save() )
-  await Promise.all( promiseArray )
-} )
+beforeEach(async () => {
+  await Note.deleteMany({})
+  await Note.insertMany(helper.initialNotes)
+})
 
 test( 'notes are returned as json', async () => {
   await api
